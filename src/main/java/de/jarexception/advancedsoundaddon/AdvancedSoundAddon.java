1bc2d487a2f7bc6f8751c493ceda86c88dc04843
JarException <76660726+JarException@users.noreply.github.com>
2026-08-29 15:45:01 +0200

    chore: bump version to 1.0.1

--- src/main/java/de/jarexception/advancedsoundaddon/AdvancedSoundAddon.java
+++ src/main/java/de/jarexception/advancedsoundaddon/AdvancedSoundAddon.java
@@ -23,7 +23,7 @@ import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 public final class AdvancedSoundAddon {
     public static final String MOD_ID = "advancedsoundaddon";
     public static final String NAME = "Advanced Sound Addon";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     private static volatile CommonProxy proxy;
 
